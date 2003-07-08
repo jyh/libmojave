c7fc01a89bdcf69b867ae49fe99bd0f96397f2c8
jyh <jyh@5109f7fa-8300-0410-942d-a7a480986ffa>
2003-07-08 00:02:55 +0000

    Moved Array_util to Lm_array_util.

--- stdlib/lm_array_util.mli
+++ stdlib/lm_array_util.mli
@@ -25,6 +25,47 @@
  * @end[license]
  *)
 
+(* Membership in an array *)
+val mem : 'a -> 'a array -> bool
+val index : 'a -> 'a array -> int
+val exists : ('a -> bool) -> 'a array -> bool
+val find_index : ('a -> bool) -> 'a array -> int
+
+(* Raises Failure *)
+val iter2 : ('a -> 'b -> unit) -> 'a array -> 'b array -> unit
+
+(* Test boolean values *)
+val all_true : bool array -> bool
+val exists_true : bool array -> bool
+val for_all : ('a -> bool) -> 'a array -> bool
+val exists : ('a -> bool) -> 'a array -> bool
+
+(*
+ * replace A i j B creates a copy of array A
+ * where j elements with indices from i to i + j - 1 are
+ * replaced with B's elements (if B has length different from j,
+ * new array will have length different from A).
+ * Raises invalid_argument if numbers out of range.
+ *)
+val replace : 'a array -> int -> int -> 'a list -> 'a array
+
+val append_list : 'a array -> 'a list -> 'a array
+val append_list_array : 'a array -> 'a list -> 'a array -> 'a array
+
+(*
+ * Map over a sub-array.
+ *)
+val sub_map : ('a -> 'b) -> 'a array -> int -> int -> 'b array
+
+(*
+ * This function builds arrays out of sub-arrays.
+ *)
+type ('a, 'b) array_part =
+   ArrayElement of 'a
+ | ArrayArray of 'b * int * int
+
+val collect : ('a, 'a array) array_part list -> 'a array
+
 (*
  * Sorts an array, than eliminates the duplicate elements
  * and moves the remaining elements into an initial segment
