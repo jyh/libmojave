494fa92a41d32f5cae3c911e07852ceb2660b32e
nogin <nogin@5109f7fa-8300-0410-942d-a7a480986ffa>
2006-06-05 23:48:48 +0000

    Added some code for copying the output of the Format/Lm_format functions to a log file.
    Currently the code for actually activating the copying is commented
    out, but if we decide that it's useful, we might want to activate it based on
    some environment variable.

--- stdlib/lm_printf.ml
+++ stdlib/lm_printf.ml
@@ -3,7 +3,7 @@
  *
  * ----------------------------------------------------------------
  *
- * Copyright (C) 2000-2005 Jason Hickey, Caltech
+ * Copyright (C) 2000-2006 Mojave Group, Caltech
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -19,10 +19,102 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- * Author: Jason Hickey
- * jyh@cs.caltech.edu
+ * Author: Jason Hickey <jyh@cs.caltech.edu>
+ * Modified By: Aleksey Nogin <nogin@cs.caltech.edu>
  *)
 include Format
+
+(*
+ * Redirect formatter's output to both a channel and a log file.
+ * The log file will be appended (not truncated), lockf-mutexed,
+ * and all the entries will be annotated with the PID of the logger:
+ * [|PID1: string1|][|PID2: string2|]...
+ * (with consequetive non-NL strings from the same PID merged together).
+ *)
+let extra_formatting file chan formatter =
+   let log = open_out_gen [Open_wronly; Open_append; Open_creat] 0o600 file in
+   let pid_file = file ^ ".pid" in
+   let pid_fd = Unix.openfile pid_file [Unix.O_RDWR; Unix.O_CREAT] 0o666 in
+   let get_pid () =
+      ignore (Unix.lseek pid_fd 0 Unix.SEEK_SET);
+      Unix.lockf pid_fd Unix.F_LOCK 0;
+      try
+         let s = String.make 10 ' ' in
+         let i = Unix.read pid_fd s 0 10 in
+         let s = String.sub s 0 i in
+            int_of_string s
+      with _ ->
+         0
+   in
+   let write_pid i =
+      ignore (Unix.lseek pid_fd 0 Unix.SEEK_SET);
+      let () = Unix.ftruncate pid_fd 0 in
+      let s = string_of_int i in
+         ignore (Unix.single_write pid_fd s 0 (String.length s));
+         ignore (Unix.lseek pid_fd 0 Unix.SEEK_SET);
+         Unix.lockf pid_fd Unix.F_ULOCK 0
+   in
+   let flush () =
+      flush chan;
+      flush log
+   in
+   let do_newline nl =
+      let old_pid = get_pid () in
+      if old_pid <> 0 then output_string log "|]";
+      output_string chan nl;
+      output_string log "\n";
+      flush ();
+      write_pid 0
+   in
+   let rec out s i len =
+      if len > 0 then begin
+         let s = String.sub s i len in
+            if String.contains s '\r' then begin
+               let i = String.index s '\r' in
+                  out s 0 i;
+                  if (i < len - 1) && s.[i+1] = '\n' then begin
+                     do_newline "\n";
+                     out s (i+2) (len - i - 2)
+                  end else begin
+                     do_newline "\r";
+                     out s (i+1) (len - i - 1)
+                  end
+            end else if String.contains s '\n' then begin
+               let i = String.index s '\n' in
+                  out s 0 i;
+                  do_newline "\n";
+                  out s (i+1) (len - i - 1)
+            end else begin
+               output_string chan s;
+               let old_pid = get_pid () in
+               let pid = Unix.getpid () in
+               if pid <> old_pid then begin
+                  if old_pid <> 0 then output_string log "|]";
+                  output_string log (sprintf "[|%i: " pid)
+               end;
+               output_string log s;
+               write_pid pid
+            end
+      end
+   in
+   let newline () = do_newline "\n" in
+   let spaces i =
+      let s = String.make i ' ' in
+         out s 0 i;
+   in
+      pp_set_all_formatter_output_functions formatter ~out ~flush ~newline ~spaces
+
+(*
+ * Copy the std_formatter and err_formatter outputs to log files.
+ *
+ * XXX: TODO: for now this is commented out, we might consider doing
+ * this based on an environment variable.
+ 
+let () =
+   extra_formatting "/tmp/mylog.out" stdout std_formatter;
+   extra_formatting "/tmp/mylog.err" stderr err_formatter
+
+ *)
 
 (*
  * For now, just use normal output channels.
